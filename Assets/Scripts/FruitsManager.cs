6e9aac3cfe096b4e6af79352608ede04b5617d8d
KOMATSU CHIKA <128227320+KOMATSU-CHIKA@users.noreply.github.com>
2024-10-12 15:12:37 +0900

    .gitignore
    設定

---
+++ Assets/Scripts/FruitsManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitsMAnager : MonoBehaviour
+{
+    public enum fruits
+    {
+        apple,
+        banana,
+        persimmon, //Š`
+        peach,
+        pear, //—œ
+        max
+    }
+}
