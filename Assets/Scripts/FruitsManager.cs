7635337acdd7ea967203007bc05fe383573d99db
KOMATSU CHIKA <128227320+KOMATSU-CHIKA@users.noreply.github.com>
2024-10-12 15:19:12 +0900

    o
    フルーツ

--- Assets/Scripts/FruitsManager.cs
+++ Assets/Scripts/FruitsManager.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FruitsMAnager : MonoBehaviour
+public class FruitsManager : MonoBehaviour
 {
+    public static FruitsManager instance;
+
     public enum fruits
     {
         apple,
@@ -12,5 +14,17 @@ public class FruitsMAnager : MonoBehaviour
         peach,
         pear, //—œ
         max
+    }
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 }
